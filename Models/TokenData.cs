ffc49dee1f626cb238c12507238f15cff6e27c94
Electyctz <email>
2025-10-29 21:42:43 +0100

    cleanup

--- Models/TokenData.cs
+++ Models/TokenData.cs
@@ -9,11 +9,5 @@ namespace Automatic_Replay_Buffer.Models
     public class TokenData
     {
         public string AccessToken { get; set; }
-
-        //public DateTime FetchedAt { get; set; } = DateTime.UtcNow;
-        //public DateTime ExpiresAt => FetchedAt.AddSeconds(ExpiresIn);
-        //public TimeSpan TimeRemaining => ExpiresAt - DateTime.UtcNow;
-
-        //public bool IsExpired => TimeRemaining.TotalMinutes <= 5;
     }
 }
